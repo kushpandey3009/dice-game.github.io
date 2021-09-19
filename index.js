bfa50a9a2a93738d428f2fe34787faf778661d82
kushpandey3009 <67239051+kushpandey3009@users.noreply.github.com>
2021-09-19 15:26:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var randomnumber1= Math.floor(Math.random() *6)+1;
 
-var rimage1="images/dice"+randomnumber1+".png";
+var rimage1="dice"+randomnumber1+".png";
 
 document.querySelector("img.img1").setAttribute("src",rimage1);
 
 var randomnumber2= Math.floor(Math.random() *6)+1;
 
-var rimage2="images/dice"+randomnumber2+".png";
+var rimage2="dice"+randomnumber2+".png";
 
 document.querySelector("img.img2").setAttribute("src",rimage2);
 if (randomnumber1>randomnumber2){
